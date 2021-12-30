3444b8bd2387fc1f46aee3b4a1773a609a35c9fe
Cronus Qiu <28886115+QiuCronus@users.noreply.github.com>
2021-12-30 15:11:44 +0800

    Update install_agent.sh

--- install_agent.sh
+++ install_agent.sh
@@ -10,11 +10,13 @@ mkdir -p $root/logs
 cd $root
 if [ ! -d "$root/venv/bin/python" ]; then
 	python -m venv $root/venv
+        $root/venv/bin/pip install -q --upgrade pip
+	$root/venv/bin/pip -V
 fi
 
 cd $root/packages
 wget https://github.com/QiuCronus/openlab/releases/download/v1.1.1/openlab_agent-1.1.1-py3-none-any.whl
 
 cd $root
-venv/bin/pip install $root/packages/openlab_agent-1.1.1-py3-none-any.whl
+venv/bin/pip install -q $root/packages/openlab_agent-1.1.1-py3-none-any.whl
 venv/bin/pip list | grep openlab
