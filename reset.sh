0267c98acd70c13a74047c790809328400f393b2
Cronus Qiu <28886115+QiuCronus@users.noreply.github.com>
2021-12-30 17:35:29 +0800

    Create reset.sh

---
+++ reset.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+dbuser=$1
+dbpass=$2
+dbname=$3
+
+mysql -u'$dbuser' -p'$dbpass' -D '$dbname' -e'update im_self_account set is_gathered = 0;'
+mysql -u'$dbuser' -p'$dbpass' -D '$dbname' -e'update im_task set is_gathered = 0;'
