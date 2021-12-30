479b8b789c082df4a941abdcf991654aec49090c
Cronus Qiu <28886115+QiuCronus@users.noreply.github.com>
2021-12-30 14:10:31 +0800

    Update install_agent.sh

--- install_agent.sh
+++ install_agent.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
-mkdir -p /home/auser/TRClient/Agent/OpenLab
-mkdir -p /home/auser/TRClient/Agent/OpenLab/packages
-mkdir -p /home/auser/TRClient/Agent/OpenLab/configs
-mkdir -p /home/auser/TRClient/Agent/OpenLab/logs
+root=/home/auser/TRClient/Agent/OpenLab
 
+mkdir -p $root
+mkdir -p $root/configs
+mkdir -p $root/packages
+mkdir -p $root/logs
 
-cd /home/auser/Agent/TRClient/OpenLab
+cd $root
 if [ ! -d "./venv/bin/python" ]; then
 	python -m venv venv
 fi
 
-cd /home/auser/Agent/TRClient/OpenLab/packages
+cd $root/packages
 wget https://github.com/QiuCronus/openlab/blob/main/openlab_agent-1.1.1-py3-none-any.whl -o openlab_agent-1.1.1-py3-none-any.whl
 
-cd /home/auser/TRClient/Agent/OpenLab
-venv/bin/pip install /home/auser/TRClient/Agent/OpenLab/packages/openlab_agent-1.1.1-py3-none-any.whl
+cd $root
+venv/bin/pip install $root/packages/openlab_agent-1.1.1-py3-none-any.whl
 venv/bin/pip list | grep openlab
